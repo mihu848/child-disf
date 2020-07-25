ddb5e9c80dec052449fa0978cf4af11cf0cf3144
Michael Huang <57814416+mihu848@users.noreply.github.com>
2020-07-25 13:11:05 -0700

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,41 @@
+
+import sys, argparse, os
+import glob
+import pandas as pd 
+import numpy as np
+import csv
+from collections import Counter
+
+def filler_rate(t1):
+    first = pd.read_csv(t1, sep='\t')
+    x=0
+    tags = []
+    fillct = []
+    totct = []
+    rate = []
+    #for x in range(len(first)):
+    while(x<len(first)):
+        key = first.loc[x, "filename"]
+        count = 0
+        tot = 0
+        while(x<len(first) and first.loc[x, "filename"]==key):
+            f=first.loc[x, "filler"]
+            if(f==1):
+                count+=1
+                tot+=1
+            else:
+                tot+=1
+            x+=1
+        print(key)
+        tags.append(key)
+        print("Fillers: " + str(count))
+        fillct.append(count)
+        print("Total Tokens: " + str(tot))
+        totct.append(tot)
+        print("Filler rate: " + str(count/tot*100)+"%")
+        rate.append(count/tot)
+        x+=1
+    df = pd.DataFrame(list(zip(tags,fillct,totct,rate)),
+                      columns = ["filename", "fillers", "tokens", "rate"])
+    df.to_csv("fillerfile.tsv", sep="\t", index=False)
+    
