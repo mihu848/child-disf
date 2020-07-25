114e4d0776931e6e7d531b7bd8fcb8cbd49b8fee
Michael Huang <57814416+mihu848@users.noreply.github.com>
2020-07-25 14:41:15 -0700

    Add files via upload

--- stats.py
+++ stats.py
@@ -57,3 +57,55 @@ def gender_filler(t1):
             females+=1
     print("Male Filler Rate: " + str(m_fill/males) + " across " + str(males)+ " subjects")
     print("Female Filler Rate: " + str(f_fill/females) + " across " + str(females)+ " subjects")
+
+#exports csv with repair data
+def disf_rate(t1, name):
+    first = pd.read_csv(t1, sep='\t')
+    x=0
+    tags = []
+    disfct = []
+    totct = []
+    rate = []
+    #for x in range(len(first)):
+    while(x<len(first)):
+        key = first.loc[x, "filename"]
+        count = 0
+        tot = 0
+        while(x<len(first) and first.loc[x, "filename"]==key):
+            f=first.loc[x, "disf"]
+            if(f==1):
+                count+=1
+                tot+=1
+            else:
+                tot+=1
+            x+=1
+        print(key)
+        tags.append(key)
+        print("Disfluencies: " + str(count))
+        disfct.append(count)
+        print("Total Tokens: " + str(tot))
+        totct.append(tot)
+        print("Disfluency rate: " + str(count/tot*100)+"%")
+        rate.append(count/tot)
+        x+=1
+    df = pd.DataFrame(list(zip(tags,disfct,totct,rate)),
+                      columns = ["filename", "disfluencies", "tokens", "disfrate"])
+    outfile = name + ".tsv"
+    df.to_csv(outfile, sep="\t", index=False)
+
+#reads in master tsv, generates repair stats by gender
+def gender_disf(t1):
+    df = pd.read_csv(t1, sep='\t')
+    males = 0
+    females = 0
+    m_disf = 0
+    f_disf = 0
+    for x in range(len(df)):
+        if(df.loc[x, "gender"]=="M"):
+            m_disf+=df.loc[x, "disfrate"]
+            males+=1
+        else:
+            f_disf+=df.loc[x, "disfrate"]
+            females+=1
+    print("Male Disfluency Rate: " + str(m_disf/males) + " across " + str(males)+ " subjects")
+    print("Female Disfluency Rate: " + str(f_disf/females) + " across " + str(females)+ " subjects")
